63edd8d49728bd5c78ac62e0a55170d4e77fd1e7
konkong <kongkong_mail@qq.com>
2020-05-17 12:04:40 +0800

    Test the m3u8 stream to save to a file
    Test the m3u8 stream to save to a file

--- detect.py
+++ detect.py
@@ -66,8 +66,7 @@ def detect(save_img=False):
         torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
         dataset = LoadStreams(source, img_size=img_size)
     else:
-        view_img = True
-        #save_img = True
+        save_img = True
         dataset = LoadImages(source, img_size=img_size)
 
     # Get names and colors
